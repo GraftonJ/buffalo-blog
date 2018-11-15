893c7d18d7264dab07d24315597ad3db2e6f4ba1
Josh <grafton.josh@gmail.com>
2018-11-15 10:57:16 -0700

    added Bootstrap 4, updated asset files

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,7 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
-require("bootstrap/dist/js/bootstrap.bundle.js");
+require("popper.js/dist/popper.min.js");
+require("bootstrap/dist/js/bootstrap.min.js");
+
 $(() => {
 
 });
