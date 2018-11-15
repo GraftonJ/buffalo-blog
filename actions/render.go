1d221f745413bd7f3f5785307aeafa873ab9abb1
Josh <grafton.josh@gmail.com>
2018-11-15 13:57:43 -0700

    working user registration

--- actions/render.go
+++ actions/render.go
@@ -1,6 +1,8 @@
 package actions
 
 import (
+	"html/template"
+
 	"github.com/gobuffalo/buffalo/render"
 	"github.com/gobuffalo/packr"
 )
@@ -12,16 +14,14 @@ func init() {
 	r = render.New(render.Options{
 		// HTML layout to be used for all HTML requests:
 		HTMLLayout: "application.html",
-
 		// Box containing all of the templates:
 		TemplatesBox: packr.NewBox("../templates"),
 		AssetsBox:    assetsBox,
-
 		// Add template helpers here:
 		Helpers: render.Helpers{
-			// uncomment for non-Bootstrap form helpers:
-			// "form":     plush.FormHelper,
-			// "form_for": plush.FormForHelper,
+			"csrf": func() template.HTML {
+				return template.HTML("<input name=\"authenticity_token\" value=\"<%= authenticity_token %>\" type=\"hidden\">")
+			},
 		},
 	})
 }
